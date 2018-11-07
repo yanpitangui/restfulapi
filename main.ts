58329b32066ec5b79137be23d8adb63bb75785e5
Yan Carlos Cordeiro Pitangui <yan.pitangui@cscservicos.com.br>
2018-11-07 14:19:59 -0200

    Fim seção 3

--- main.ts
+++ main.ts
@@ -1,35 +1,11 @@
-import * as restify from 'restify';
+import {Server} from "./server/server";
+import {usersRouter} from "./users/users.router";
 
-const server = restify.createServer({
-    "name": "meat-api",
-    "version": "1.0.0"
-});
-
-server.use(restify.plugins.queryParser());
-
-server.get("/info", [
-    (req, resp, next) => {
-        if(req.userAgent() && req.userAgent().includes('MSIE 7.0')){
-            resp.status(400);
-            resp.json({message: "Please, update your browser."})
-            return next(false);
-        }
-        return next();
-    },
-
-    (req, resp, next) => {
-        resp.json({
-            browser: req.userAgent(),
-            method: req.method,
-            url: req.url,
-            path: req.path(),
-            query: req.query
-
-        });
-        return next();
-    }
-]);
-
-server.listen(3000, () => {
-    console.log("API is running on http://localhost:3000");
-});
+const server = new Server();
+server.bootstrap([usersRouter]).then(server=>{
+    console.log(`Server is listening on localhost:${server.application.address().port}`)
+}).catch(error=>{
+    console.log("Server failed to start.");
+    console.error(error);
+    process.exit(1);
+})
